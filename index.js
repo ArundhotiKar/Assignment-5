614cf910c163d6d74319ac4e6724e4ad96a2d857
ArundhotiKar <arundhotikar60@gmail.com>
2025-08-27 20:57:07 +0600

    Responsive Done

--- index.js
+++ index.js
@@ -19,12 +19,33 @@ cards.forEach((card, index) => {
   copyBtn.addEventListener("click", function() {
     copyCount++;
     availableCopyCount.innerText=copyCount;
-    console.log("Copy button clicked in card:", index + 1);
+    //console.log("Copy button clicked in card:", index + 1);
     const number = card.querySelector("h1.text-2xl").innerText;
     navigator.clipboard.writeText(number);
     alert(`Copied: ${number}`);
   });
 });
+
+
+// Functionality of call button
+const coinCount = document.getElementById('coin-count');
+cards.forEach((card, index) => {
+  const callBtn = card.querySelector(".call-btn");
+  callBtn.addEventListener("click", function() {
+    let currentCoin = parseInt(coinCount.innerText);
+
+    if (currentCoin < 20) {
+      alert("You don't have enough coins. You need at least 20 coins to make a call");
+      return;
+    }
+    currentCoin -= 20;
+    coinCount.innerText = currentCoin;
+    const serviceType = card.querySelector("h1.calling").innerText;
+    const number = card.querySelector("h1.text-2xl").innerText;
+    alert(`Calling ${serviceType} ${number}...`);
+  });
+});
+
 
 
 
